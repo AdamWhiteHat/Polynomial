38f0b0cafc8ee4a96d79693714fa4039fcc37c91
AdamRakaska <adamwhitehat@outlook.com>
2018-12-10 08:24:45 -0700

    Added ChineseRemainderTheorem

--- SparsePolynomial/MiscMaths.cs
+++ SparsePolynomial/MiscMaths.cs
@@ -132,5 +132,33 @@ namespace SparsePolynomialLibrary
 			return r;
 		}
 
+		/// <summary>
+		/// Finds the modulus such that each N, mod the mystery modulus, equal each corresponding A
+		/// </summary>
+		public static BigInteger ChineseRemainderTheorem(BigInteger[] n, BigInteger[] a)
+		{
+			BigInteger prod = n.Aggregate(BigInteger.One, (i, j) => i * j);
+			BigInteger p;
+			BigInteger sm = 0;
+			for (int i = 0; i < n.Length; i++)
+			{
+				p = prod / n[i];
+				sm += a[i] * ModularMultiplicativeInverse(p, n[i]) * p;
+			}
+			return sm % prod;
+		}
+
+		private static BigInteger ModularMultiplicativeInverse(BigInteger a, BigInteger mod)
+		{
+			BigInteger b = a % mod;
+			for (int x = 1; x < mod; x++)
+			{
+				if ((b * x) % mod == 1)
+				{
+					return x;
+				}
+			}
+			return 1;
+		}
 	}
 }
